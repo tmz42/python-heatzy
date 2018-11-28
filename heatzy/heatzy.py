d7e6ea41b96330bdecc4d7d370f86427f2553ac1
Thomas MONZIE <thomas.monzie@gmail.com>
2018-11-28 11:53:25 +0100

    Addec main function

--- heatzy/heatzy.py
+++ heatzy/heatzy.py
@@ -1,11 +1,14 @@
 import requests
 import argparse
+import sys
 
 # Constantes
 heatzy_api_base_url = "https://euapi.gizwits.com/app"
 heatzy_appID = "c70a66ff039d41b4a220e198b0fcc8b3"
 
 # Matrice de décodage des modes
+available_modes = ('OFF', 'ECO', 'HGEL', 'CONFORT')
+
 modes_decode = {
     'Pilote2' : {'stop' : 'OFF', 'eco' : 'ECO', 'fro' : 'HGEL', 'cft' : 'CONFORT'},
     'Heatzy' : {'停止' : 'OFF', '经济' : 'ECO', '解冻' : 'HGEL', '舒适' : 'CONFORT'}
@@ -34,8 +37,13 @@ class HeatzyHandler:
         login_headers = {'Accept': 'application/json', 'X-Gizwits-Application-Id': heatzy_appID}
         login_payload = {'username': self.login, 'password': self.password, 'lang': 'en'}
         loginRequest = requests.post(heatzy_api_base_url+'/login', json=login_payload, headers=login_headers)
-        self.token = loginRequest.json()['token']
-        return self.token
+
+        if 'token' in loginRequest.json():
+            self.token = loginRequest.json()['token']
+            return self.token
+
+        else:
+            raise Exception('Erreur de login : '+loginRequest.json())
 
     # Récupère les devices
     def getHeatzyDevices(self):
@@ -43,14 +51,14 @@ class HeatzyHandler:
         loginRequest = requests.get(heatzy_api_base_url+'/bindings', headers=login_headers)
 
         request_devices_list = loginRequest.json()['devices']
-        devices_list = list()
+        devices_dict = dict()
 
         # Infos à extraire : device alias, did, product_name
         for device in request_devices_list:
             dev = HeatzyDevice(self,name=device['dev_alias'], did=device['did'], version=device['product_name'])
-            devices_list.append(dev)
+            devices_dict[dev.name] = dev
 
-        return devices_list
+        return devices_dict
 
 # Classe HeatzyDevice
 class HeatzyDevice:
@@ -64,7 +72,7 @@ class HeatzyDevice:
 
     # ToString
     def __str__(self):
-        str = 'HeatzyDevice : name:'+self.name+',did:'+self.did+',version:'+self.version+',mode:'+self.mode
+        str = 'HeatzyDevice : name:'+self.name+',did:'+self.did+',version:'+self.version+',mode:'+self.status()
         return str
 
     # Rafraichit l'etat
@@ -105,21 +113,35 @@ def main():
     parser.add_argument('-p', '--password', help="Password of the user")
     parser.add_argument('-d', '--device', help="Name of the Heatzy device you wish to control")
     parser.add_argument('-l', '--list', help="List all devices", action="count", default=0)
-    parser.add_argument('--setmode', help="Sets the mode of the device")
+    parser.add_argument('-m', '--setmode', help="Sets the mode of the device")
 
     args = parser.parse_args()
 
     # On liste les devices
-    if args.list:
+    if args.list and args.username and args.password:
         hh = HeatzyHandler(args.username, args.password)
         deviceList = hh.getHeatzyDevices()
 
-        for device in deviceList:
-            print(device)
+        for device_name in deviceList:
+            print(device_name+deviceList[device_name].__str__())
 
-    # On définit le mode
-    if args.setmode:
-        pass
+    # On recherche un device
+    elif args.device and args.username and args.password:
+        hh = HeatzyHandler(args.username, args.password)
+        deviceList = hh.getHeatzyDevices()
+
+        if args.device in deviceList:
+            if args.setmode:
+                if args.setmode in available_modes:
+                    deviceList[args.device].setMode(args.setmode)
+                else:
+                    print('Mode not found', file=sys.stderr)
+            print(deviceList[args.device].__str__())
+        else:
+            print('Device "'+args.device+'" not found', file=sys.stderr)
+
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
