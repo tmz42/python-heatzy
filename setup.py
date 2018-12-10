7b6bacbd1eeaf1f0848c6d4f008808d1ef786c4f
Thomas MONZIE <thomas.monzie@gmail.com>
2018-12-10 16:46:00 +0100

    Edited version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="heatzy",
-    version="0.0.4",
+    version="0.0.5",
     author="Thomas MONZIE",
     author_email="thomas.monzie@gmail.com",
     description="Controller for Heatzy products",
